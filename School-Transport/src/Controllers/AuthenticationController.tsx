31a1149da84d8048838e27024308795b9ab340b7
Warona Mogolwane <warona@myactive.co.za>
2023-10-02 22:01:13 +0200

    Added Authentication Controller
    Added firebase authentication functions
    
    Added a sign up page
    
    Added an authentication response model

---
+++ School-Transport/src/Controllers/AuthenticationController.tsx
@@ -0,0 +1,23 @@
+import { 
+  CreateUserWithEmailPassword, 
+  LoginWithEmailPassword,
+  ForgotPassword,
+  SetPassword,
+  VerifyEmail
+} from "../Firebase/Authentication";
+
+export const UserSignUp = async (userEmail: string, userPassword: string) => {
+   return await CreateUserWithEmailPassword(userEmail, userPassword);
+};
+export const UserSignIn = async (userEmail: string, userPassword: string) => {
+   return await LoginWithEmailPassword(userEmail, userPassword);
+};
+export const UserForgotPassword = async (userEmail: string) => {
+   return await ForgotPassword(userEmail);
+};
+export const UserSetPassword = async (userPassword: string) => {
+   return await SetPassword(userPassword);
+};
+export const UserVerifyEmail = async () => {
+   return await VerifyEmail();
+};

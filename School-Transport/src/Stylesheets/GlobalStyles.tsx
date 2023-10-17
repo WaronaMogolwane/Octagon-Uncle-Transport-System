2209cbf421bbbd55e4b077a5928aa5dada40d315
Warona Mogolwane <warona@myactive.co.za>
2023-10-17 11:01:27 +0200

    Added components and styles

---
+++ School-Transport/src/Stylesheets/GlobalStyles.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, View } from 'react-native';
+
+export const FormStyles = () => {
+    StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: '#fff',
+            alignItems: 'center',
+            justifyContent: 'center',
+
+        },
+        input: {
+            width: '75%',
+            marginBottom: 16
+        },
+        logo: {
+            marginBottom: 32
+        }
+    })
+}

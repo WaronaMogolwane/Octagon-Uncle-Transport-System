4b1e04f082f3072351a5843213290d8ccebbaef0
Warona Mogolwane <warona@myactive.co.za>
2023-10-24 00:58:58 +0200

    Added node js server

---
+++ Server/src/Middleware/auth-helper.js
@@ -0,0 +1,51 @@
+const { GetUserByEmail, AddNewUser } = require('./../Middleware/database-helper.js')
+
+async function CheckIfUserExists(req, res, next) {
+    if (await GetUserByEmail(req.body.userDetails.Email)) {
+        res
+            .status(400)
+            .send('User account already exists');
+    }
+    else {
+        next();
+    }
+}
+
+function RegisterUser(req, res, next) {
+    let user = {
+        FirstName: req.body.userDetails.FirstName,
+        LastName: req.body.userDetails.LastName,
+        Email: req.body.userDetails.Email,
+        Phone: req.body.userDetails.Phone,
+        Password: req.body.userDetails.Password,
+        AvatarUrl: 'NULL'
+    }
+    AddNewUser(user).then(
+        () => {
+            res.status(200)
+                .json(
+                    {
+                        UserCreated: true,
+                    }
+                );
+        },
+        (error) => {
+            if (error.errno === 1062) {
+                res.status(491)
+                .json({ 
+                    UserCreated: false,
+                    error: 'User already exists' 
+                });
+            }
+            else {
+                res.status(400).
+                json({ 
+                    UserCreated: false,
+                    error: 'There was an erorr while creating the user' 
+                });
+            }
+        }
+    );
+}
+
+module.exports = { CheckIfUserExists, RegisterUser };

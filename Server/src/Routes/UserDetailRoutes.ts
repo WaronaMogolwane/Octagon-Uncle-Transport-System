1fe96e08a7e98c79747af8f7fee975f6272dda7b
Warona Mogolwane <warona@myactive.co.za>
2023-10-27 01:26:44 +0200

    Typescript and Modules compatibility

--- Server/src/Routes/UserDetailRoutes.ts
+++ Server/src/Routes/UserDetailRoutes.ts
@@ -6,14 +6,14 @@ import {
 } from "../Controllers/UserDetailController";
 const router = Router();
 
-router.post("/add-user-details", AddUserDetail, async (req, res, next) => {});
+router.post("/add-user-details", AddUserDetail, async (req, res, next) => { });
 
-router.post("/get-user-details", GetUserDetail, async (req, res, next) => {});
+router.post("/get-user-details", GetUserDetail, async (req, res, next) => { });
 
 router.patch(
   "/update-user-details",
   UpdateUserPersonalDetail,
-  async (req, res, next) => {}
+  async (req, res, next) => { }
 );
 
 export default router;

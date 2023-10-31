682118b09d8ce2a9ac4b355e443d614cdf76cc8f
Warona Mogolwane <warona@myactive.co.za>
2023-10-31 13:56:09 +0200

    Implemented promise routing

--- Server/src/Routes/AuthenticationRoutes.ts
+++ Server/src/Routes/AuthenticationRoutes.ts
@@ -1,14 +1,16 @@
-import { Router } from "express";
+//import { Router } from "express";
+import Router from 'express-promise-router'
 import { AddNewUser, GetUserByEmail } from "../Models/DatabaseModel";
 import { SendOtp, VerifyOtp } from "../Models/OtpModel";
 import {
   CheckIfUserExists,
   RegisterUser,
 } from "../Controllers/AuthenticationController";
+import { DbConnect } from "../Services/DatabaseService"
 import { authenticateJWT } from "../Middleware/Auth";
 const router = Router();
 
-router.post("/register-user", RegisterUser, (req, res, next) => { });
+router.post("/register-user", DbConnect, (req, res, next) => { });
 
 router.post("/login", (req, res) => { });
 

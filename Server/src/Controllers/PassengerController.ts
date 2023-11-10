b0252545448912f85567c3c15fc789ad26056161
Iamtallers <iamtallers@gmail.com>
2023-11-10 12:37:51 +0200

    Updated Passenger Controller, Model, Class, Routes

--- Server/src/Controllers/PassengerController.ts
+++ Server/src/Controllers/PassengerController.ts
@@ -1,6 +1,12 @@
 import { randomUUID } from "crypto";
 import { Passenger } from "../Classes/Passenger";
-import { AddPassengerData } from "../Models/PassengerModel";
+import {
+  AddPassengerData,
+  GetAllPassengerUserData,
+  GetAllPassengerBusinessData,
+  GetPassengerData,
+  UpdatePassengerData,
+} from "../Models/PassengerModel";
 
 export const AddPassenger = async (req: any, res: any, next: any) => {
   let newPassenger = new Passenger(
@@ -8,9 +14,10 @@ export const AddPassenger = async (req: any, res: any, next: any) => {
     req.body.passenger.FirstName,
     req.body.passenger.LastName,
     req.body.passenger.Age,
-    req.body.passenger.Homeaddress,
-    req.body.passenger.Destinationaddress,
-    req.body.passenger.UserId
+    req.body.passenger.HomeAddress,
+    req.body.passenger.DestinationAddress,
+    req.body.passenger.UserId,
+    req.body.passenger.BusinessDetailId
   );
   await AddPassengerData(newPassenger, (error, result) => {
     if (error) {
@@ -26,4 +33,99 @@ export const AddPassenger = async (req: any, res: any, next: any) => {
       });
     }
   });
+};
+
+export const GetPassenger = async (req: any, res: any, next: any) => {
+  let passengerId = req.body.passenger.PassengerId;
+
+  await GetPassengerData(passengerId, (error, result) => {
+    if (error) {
+      let err: any = {
+        status: error.status,
+        message: error.message,
+      };
+      next(err);
+    } else {
+      res.status(200).json({
+        RecordRetrieved: true,
+        result: result.rows[0],
+      });
+      console.log(result);
+    }
+  });
+};
+
+export const UpdatePassenger = async (req: any, res: any, next: any) => {
+  let passenger = new Passenger(
+    req.body.passenger.PassengerId,
+    req.body.passenger.FirstName,
+    req.body.passenger.LastName,
+    req.body.passenger.Age,
+    req.body.passenger.HomeAddress,
+    req.body.passenger.DestinationAddress,
+    req.body.passenger.UserId,
+    req.body.passenger.BusinessDetailId
+  );
+  await UpdatePassengerData(passenger, (error, result) => {
+    if (error) {
+      let err: any = {
+        status: error.status,
+        message: error.message,
+      };
+      next(err);
+    } else {
+      res.status(200).json({
+        UserDetailUpdated: true,
+        result: result.rows[0],
+      });
+    }
+  });
+};
+
+export const GetAllUserPassenger = async (req: any, res: any, next: any) => {
+  let passengerId = req.body.passenger.PassengerId;
+  let userId = req.body.passenger.UserId;
+
+  await GetAllPassengerUserData(passengerId, userId, (error, result) => {
+    if (error) {
+      let err: any = {
+        status: error.status,
+        message: error.message,
+      };
+      next(err);
+    } else {
+      res.status(200).json({
+        RecordRetrieved: true,
+        result: result.rows[0],
+      });
+    }
+  });
+};
+
+export const GetAllBusinessPassenger = async (
+  req: any,
+  res: any,
+  next: any
+) => {
+  let passengerId = req.body.passenger.PassengerId;
+  let businessDetailId = req.body.passenger.BusinessDetailId;
+
+  await GetAllPassengerBusinessData(
+    passengerId,
+    businessDetailId,
+    (error, result) => {
+      if (error) {
+        let err: any = {
+          status: error.status,
+          message: error.message,
+        };
+        next(err);
+      } else {
+        res.status(200).json({
+          RecordRetrieved: true,
+          result: result.rows[0],
+        });
+      }
+    }
+  );
 };
--- Server/src/Models/PassengerModel.ts
+++ Server/src/Models/PassengerModel.ts
@@ -9,15 +9,16 @@ export const AddPassengerData = async (
 ) => {
   DbClient.connect((err) => {
     DbClient.query(
-      "CALL public.sp_insert_new_passenger($1::text,$2::text,$3::text,$4::integer,$5::text,$6::text,$7::text)",
+      "CALL public.sp_insert_new_passenger($1::text,$2::text,$3::text,$4::integer,$5::text,$6::text,$7::text,$8::text)",
       [
         passenger.passenger_id,
         passenger.firstName,
         passenger.lastName,
         passenger.age,
-        passenger.homeaddress,
-        passenger.destinationaddress,
+        passenger.homeAddress,
+        passenger.destinationAddress,
         passenger.userId,
+        passenger.businessDetailId,
       ],
       (err, res) => {
         DbClient.end();
@@ -31,13 +32,13 @@ export const AddPassengerData = async (
   });
 };
 
-export const GetUserDetailData = async (
-  userId: string,
+export const GetPassengerData = async (
+  passngerId: string,
   callback: (error, result) => void
 ) => {
   DbClient.connect((err) => {
     DbClient.query(
-      `select * from sp_get_user_detail('${userId}');`,
+      `select * from sp_get_passenger('${passngerId}');`,
       (err, res) => {
         DbClient.end();
         if (err) {
@@ -50,21 +51,20 @@ export const GetUserDetailData = async (
   });
 };
 
-export const UpdateUserDetailData = async (
+export const UpdatePassengerData = async (
   passenger: Passenger,
   callback: (error, result) => void
 ) => {
   DbClient.connect((err) => {
     DbClient.query(
-      "CALL public.sp_insert_new_passenger($1::text,$2::text,$3::text,$4::text,$5::text,$6::text,$7::text)",
+      "CALL public.sp_update_passenger($1::text,$2::text,$3::text,$4::integer,$5::text,$6::text)",
       [
         passenger.passenger_id,
         passenger.firstName,
         passenger.lastName,
         passenger.age,
-        passenger.homeaddress,
-        passenger.destinationaddress,
-        passenger.userId,
+        passenger.homeAddress,
+        passenger.destinationAddress,
       ],
       (err, res) => {
         DbClient.end();
@@ -78,21 +78,42 @@ export const UpdateUserDetailData = async (
   });
 };
 
-// export const GetAllUserDetailData = async (
-//   userId: string,
-//   callback: (error, result) => void
-// ) => {
-//   DbClient.connect((err) => {
-//     DbClient.query(
-//       `select * from sp_get_user_detail('${userId}');`,
-//       (err, res) => {
-//         DbClient.end();
-//         if (err) {
-//           callback(err, null);
-//         } else {
-//           callback(null, res);
-//         }
-//       }
-//     );
-//   });
-// };
+export const GetAllPassengerBusinessData = async (
+  passengerId: string,
+  businessDetailId: string,
+  callback: (error, result) => void
+) => {
+  DbClient.connect((err) => {
+    DbClient.query(
+      `select * from sp_get_all_passenger_business('${passengerId}','${businessDetailId}');`,
+      (err, res) => {
+        DbClient.end();
+        if (err) {
+          callback(err, null);
+        } else {
+          callback(null, res);
+        }
+      }
+    );
+  });
+};
+
+export const GetAllPassengerUserData = async (
+  passengerId: string,
+  userId: string,
+  callback: (error, result) => void
+) => {
+  DbClient.connect((err) => {
+    DbClient.query(
+      `select * from sp_get_all_passenger_user('${passengerId}','${userId}');`,
+      (err, res) => {
+        DbClient.end();
+        if (err) {
+          callback(err, null);
+        } else {
+          callback(null, res);
+        }
+      }
+    );
+  });
+};

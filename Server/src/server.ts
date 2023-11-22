ffb361b7f8e0a0e46a8621d276e187323c0335e1
Iamtallers <iamtallers@gmail.com>
2023-11-22 17:44:03 +0200

    Personal details UI

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -10,15 +10,15 @@ import ErrorHandler from "./Middleware/ErrorHandler";
 
 const app = express();
 
-var cors = require('cors');
+var cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 8081;
 
 app.use("/auth", authRoute);
-app.use("/user-profile", AuthenticateJWT, userProfileRoute);
-app.use("/passenger", AuthenticateJWT, passengerRoute);
+app.use("/user-profile", userProfileRoute);
+app.use("/passenger", passengerRoute);
 
 app.listen(PORT, function () {
   console.log(`Server is live on Port ${PORT}`);

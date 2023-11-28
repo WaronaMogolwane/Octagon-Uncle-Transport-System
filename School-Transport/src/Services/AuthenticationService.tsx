c5deab80d0da6ed5d8f6781813268f36c14fa8dc
Warona Mogolwane <warona@myactive.co.za>
2023-11-28 22:58:45 +0200

    Updates

--- School-Transport/src/Services/AuthenticationService.tsx
+++ School-Transport/src/Services/AuthenticationService.tsx
@@ -1,12 +1,5 @@
 import React from "react";
 import { useStorageState } from "./StorageStateService";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  sendPasswordResetEmail,
-  updatePassword,
-  sendEmailVerification,
-} from "firebase/auth";
 import { FIREBASE_AUTH } from "../Firebase/FirebaseConfig";
 import { AuthenticationResponseModel } from "../Models/AuthenticationResponseModel";
 import axios from "axios";
@@ -82,23 +75,7 @@ export const CreateUserWithEmailPassword = async (
   userPassword: string
 ) => {
   authenticationResponse = new AuthenticationResponseModel();
-  await createUserWithEmailAndPassword(FIREBASE_AUTH, userEmail, userPassword)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      authenticationResponse.firebaseFunction =
-        "createUserWithEmailAndPassword";
-      authenticationResponse.uid = user.uid;
-      authenticationResponse.status = "Success";
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      authenticationResponse.firebaseFunction =
-        "createUserWithEmailAndPassword";
-      authenticationResponse.errorCode = errorCode;
-      authenticationResponse.errorMessage = errorMessage;
-      authenticationResponse.status = "Failed";
-    });
+
   return authenticationResponse;
 };
 
@@ -124,46 +101,18 @@ export const LoginWithEmailPassword = async (
 
 export const ForgotPassword = async (userEmail: string) => {
   authenticationResponse = new AuthenticationResponseModel();
-  await sendPasswordResetEmail(FIREBASE_AUTH, userEmail)
-    .then(() => {
-      authenticationResponse.firebaseFunction = "sendPasswordResetEmail";
-      authenticationResponse.status = "Success";
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      authenticationResponse.firebaseFunction = "sendPasswordResetEmail";
-      authenticationResponse.errorCode = errorCode;
-      authenticationResponse.errorMessage = errorMessage;
-      authenticationResponse.status = "Failed";
-    });
+
   return authenticationResponse;
 };
 
 export const SetPassword = async (newUserPassword: string) => {
   authenticationResponse = new AuthenticationResponseModel();
-  const user: any = FIREBASE_AUTH.currentUser;
-  await updatePassword(user, newUserPassword)
-    .then(() => {
-      authenticationResponse.firebaseFunction = "updatePassword";
-      authenticationResponse.status = "Success";
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      authenticationResponse.firebaseFunction = "updatePassword";
-      authenticationResponse.errorCode = errorCode;
-      authenticationResponse.errorMessage = errorMessage;
-      authenticationResponse.status = "Failed";
-    });
+
   return authenticationResponse;
 };
 
 export const VerifyEmail = async () => {
   const user: any = FIREBASE_AUTH.currentUser;
-  await sendEmailVerification(user).then(() => {
-    authenticationResponse.firebaseFunction = "sendEmailVerification";
-    authenticationResponse.status = "Success";
-  });
+
   return authenticationResponse;
 };

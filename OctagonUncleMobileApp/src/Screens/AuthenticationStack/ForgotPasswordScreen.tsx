397868d782a4d04397dd4bc68123955439a1e990
Warona Mogolwane <warona@myactive.co.za>
2023-12-12 02:34:38 +0200

    Added forgot password input and modal

--- OctagonUncleMobileApp/src/Screens/AuthenticationStack/ForgotPasswordScreen.tsx
+++ OctagonUncleMobileApp/src/Screens/AuthenticationStack/ForgotPasswordScreen.tsx
@@ -1,11 +1,99 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View} from 'react-native';
+import React, {useState} from 'react';
+import {CustomFormControlInput} from '../../Components/CustomFormInput';
+import {CustomButton1} from '../../Components/Buttons';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {ThemeStyles} from '../../Stylesheets/GlobalStyles';
+import {
+  ModalBackdrop,
+  ModalContent,
+  ModalHeader,
+  Heading,
+  ModalCloseButton,
+  Icon,
+  CloseIcon,
+  ModalBody,
+  Input,
+  InputField,
+  ModalFooter,
+  ButtonText,
+  Text,
+  Button,
+  Modal,
+} from '@gluestack-ui/themed';
 
 const ForgotPasswordScreen = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [isEmailVerified, setIsEmailVerified] = useState(false);
+
   return (
-    <View>
-      <Text>Forgot Password Screen</Text>
-    </View>
+    <SafeAreaView style={ThemeStyles.container}>
+      <CustomFormControlInput
+        labelText="Enter your"
+        placeHolder="email"
+        isInvalid={false}
+        isRequired={true}
+        type="text"
+        onChangeText={() => {}}
+        errorText={''}
+        onBlur={() => {}}
+        value={''}
+      />
+      <CustomButton1
+        title="Next"
+        onPress={() => {
+          setShowModal(true);
+        }}
+      />
+      <Modal
+        isOpen={showModal}
+        onClose={() => {
+          setShowModal(false);
+        }}>
+        <ModalBackdrop />
+        <ModalContent>
+          <ModalHeader>
+            <Heading size="lg">Email verification</Heading>
+            <ModalCloseButton>
+              <Icon as={CloseIcon} />
+            </ModalCloseButton>
+          </ModalHeader>
+          <ModalBody>
+            <Text>Enter the OTP sent to {}</Text>
+            <Input
+              variant="outline"
+              size="md"
+              isDisabled={false}
+              isInvalid={false}
+              isReadOnly={false}>
+              <InputField placeholder="Enter the code here" />
+            </Input>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              variant="outline"
+              size="sm"
+              action="secondary"
+              mr="$3"
+              onPress={() => {
+                setShowModal(false);
+              }}>
+              <ButtonText>Cancel</ButtonText>
+            </Button>
+            <Button
+              size="sm"
+              action="positive"
+              borderWidth="$0"
+              onPress={() => {
+                setShowModal(false);
+                setIsEmailVerified(true);
+              }}>
+              <ButtonText>Verify</ButtonText>
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </SafeAreaView>
   );
 };
 

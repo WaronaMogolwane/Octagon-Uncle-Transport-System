e4a0988de566364e547f38e71eec9ec358f09cfc
Warona Mogolwane <warona@myactive.co.za>
2023-12-12 02:16:34 +0200

    Added user role select to authentication stack

--- OctagonUncleMobileApp/src/Navigation/AuthenticationStack.tsx
+++ OctagonUncleMobileApp/src/Navigation/AuthenticationStack.tsx
@@ -3,13 +3,16 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SignInScreen from '../Screens/AuthenticationStack/SignInScreen';
 import SignUpPage from '../Screens/AuthenticationStack/SignUpScreen';
 import ForgotPasswordScreen from '../Screens/AuthenticationStack/ForgotPasswordScreen';
+import SelectUserRoleScreen from '../Screens/AuthenticationStack/SelectUserRoleScreen';
+import SignUpScreen from '../Screens/AuthenticationStack/SignUpScreen';
 
 const AuthenticationStack = () => {
   const Stack = createNativeStackNavigator();
   return (
     <Stack.Navigator>
       <Stack.Screen name="Sign In" component={SignInScreen} />
-      <Stack.Screen name="Sign Up" component={SignUpPage} />
+      <Stack.Screen name="User Role" component={SelectUserRoleScreen} />
+      <Stack.Screen name="Sign Up" component={SignUpScreen} />
       <Stack.Screen name="Forgot Password" component={ForgotPasswordScreen} />
     </Stack.Navigator>
   );

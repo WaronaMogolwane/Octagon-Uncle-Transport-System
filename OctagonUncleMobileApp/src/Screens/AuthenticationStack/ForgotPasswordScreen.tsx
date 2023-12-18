87d2beff46d8add2d2b19a6bfb57199848617a9e
Warona Mogolwane <warona@myactive.co.za>
2023-12-18 17:29:46 +0200

    Addded forgot password inputs

--- OctagonUncleMobileApp/src/Screens/AuthenticationStack/ForgotPasswordScreen.tsx
+++ OctagonUncleMobileApp/src/Screens/AuthenticationStack/ForgotPasswordScreen.tsx
@@ -21,30 +21,87 @@ import {
   Button,
   Modal,
 } from '@gluestack-ui/themed';
+import {useFormik} from 'formik';
+import * as yup from 'yup';
 
 const ForgotPasswordScreen = () => {
+  const registerSchema = yup.object().shape({
+    password: yup
+      .string()
+      .min(8, 'Password must be at least 8 characters')
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+        'Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character',
+      )
+      .required('Password is required'),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref('password')], 'Passwords must match'),
+  });
+
+  const registerInitialValues = {
+    password: '',
+    confirmPassword: '',
+  };
+
+  const formik = useFormik({
+    initialValues: registerInitialValues,
+    validationSchema: registerSchema,
+
+    onSubmit: (values, {resetForm}) => {},
+  });
+
   const [showModal, setShowModal] = useState(false);
   const [isEmailVerified, setIsEmailVerified] = useState(false);
 
   return (
     <SafeAreaView style={ThemeStyles.container}>
-      <CustomFormControlInput
-        labelText="Enter your"
-        placeHolder="email"
-        isInvalid={false}
-        isRequired={true}
-        type="text"
-        onChangeText={() => {}}
-        errorText={''}
-        onBlur={() => {}}
-        value={''}
-      />
-      <CustomButton1
-        title="Next"
-        onPress={() => {
-          setShowModal(true);
-        }}
-      />
+      {isEmailVerified ? (
+        <View>
+          <CustomFormControlInput
+            labelText="New password"
+            placeHolder="Password"
+            value={formik.values?.password}
+            type="password"
+            isRequired={true}
+            errorText={formik?.errors?.password}
+            isInvalid={!!formik.errors.password}
+            onChangeText={formik.handleChange('password')}
+          />
+          <CustomFormControlInput
+            labelText="Confirm password"
+            placeHolder="Password"
+            value={formik.values?.confirmPassword}
+            type="password"
+            isRequired={true}
+            errorText={formik?.errors?.confirmPassword}
+            isInvalid={!!formik.errors.confirmPassword}
+            onChangeText={formik.handleChange('confirmPassword')}
+          />
+        </View>
+      ) : (
+        <CustomFormControlInput
+          labelText="Enter your email"
+          placeHolder="email"
+          isInvalid={false}
+          isRequired={true}
+          type="text"
+          onChangeText={() => {}}
+          errorText={''}
+          onBlur={() => {}}
+          value={''}
+        />
+      )}
+      {isEmailVerified ? (
+        <CustomButton1 title="Save" onPress={() => {}} />
+      ) : (
+        <CustomButton1
+          title="Next"
+          onPress={() => {
+            setShowModal(true);
+          }}
+        />
+      )}
       <Modal
         isOpen={showModal}
         onClose={() => {

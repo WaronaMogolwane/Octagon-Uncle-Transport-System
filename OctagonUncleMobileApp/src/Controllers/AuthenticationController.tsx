9ce539a2f8f31d9d630e4c32c642bf5bb31abef1
Warona Mogolwane <warona@myactive.co.za>
2024-01-19 01:54:05 +0200

    Implemented sign up in the Authcontext provider to set session and sign in

--- OctagonUncleMobileApp/src/Controllers/AuthenticationController.tsx
+++ OctagonUncleMobileApp/src/Controllers/AuthenticationController.tsx
@@ -1,4 +1,5 @@
 import {UserInvitationModel} from '../Models/UserInvitation';
+import {User} from '../Models/UserModel';
 import {
   CreateUserWithEmailPassword,
   LoginWithEmailPassword,
@@ -7,9 +8,19 @@ import {
   VerifyUserInvitation,
 } from '../Services/AuthenticationService';
 
-export const UserSignUp = async (userEmail: string, userPassword: string) => {
-  return await CreateUserWithEmailPassword(userEmail, userPassword);
+export const UserSignUp = async (
+  user: User,
+  callback: (error: any, result: any) => void,
+) => {
+  await CreateUserWithEmailPassword(user, (error, result) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      callback(null, result);
+    }
+  });
 };
+
 export const UserSignIn = async (userEmail: string, userPassword: string) => {
   await LoginWithEmailPassword(userEmail, userPassword, (error, result) => {
     if (error) {

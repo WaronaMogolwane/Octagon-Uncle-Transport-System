e6eb645fb0a47521a4c3941f45c515fe5e14d80a
Warona Mogolwane <warona@myactive.co.za>
2024-01-19 01:58:09 +0200

    Reduced amount of passed in params

--- Server/src/Controllers/AuthenticationController.ts
+++ Server/src/Controllers/AuthenticationController.ts
@@ -17,12 +17,13 @@ export const CheckIfUserExists = async (req: any, res: any, next: any) => {
 };
 export const RegisterUser = async (req: any, res: any, next: any) => {
   let newUser = new User(
+
     randomUUID(),
     req.body.Email,
     req.body.Password,
     req.body.Cellphone,
-    req.body.Status,
-    req.body.LastLogin,
+    null,
+    null,
     req.body.UserRole
   );
 

d616e08cc49e3271697851bf9b20176461348b9b
Iamtallers <iamtallers@gmail.com>
2024-02-20 18:40:34 +0200

    Trips changes

--- Server/src/Models/TripModel.ts
+++ Server/src/Models/TripModel.ts
@@ -7,23 +7,26 @@ export const InsertTrip = async (
   trip: Trip,
   callback: (error, result) => void
 ) => {
-  await DbPool.query(
-    `CALL public.sp_insert_new_trip($1::text,$2::text,$3::json,$4::text,$5::text,$6::text,$7::date,$8::text)`,
-    [
-      trip.tripId,
-      trip.registrationNumber,
-      JSON.stringify(trip.passenger),
-      trip.vehicleId,
-      trip.businesId,
-      trip.driverId,
-      trip.date,
-      trip.time,
-    ],
-    (err, res) => {
-      if (err) {
-        callback(err, null);
+  DbPool.query(
+    {
+      sql: "CALL InsertNewTrip(?,?,?,?,?,?,?,?);",
+      timeout: 40000,
+      values: [
+        trip.tripId,
+        trip.passengerId,
+        trip.driverVehicleLinkingId,
+        trip.date,
+        trip.pickUpTime,
+        trip.dropOffTime,
+        trip.isCompleted,
+        trip.tripStatus,
+      ],
+    },
+    function (error, results, fields) {
+      if (error) {
+        callback(error, null);
       } else {
-        callback(null, res);
+        callback(null, results);
       }
     }
   );
@@ -33,61 +36,61 @@ export const GetTripById = async (
   tripId: string,
   callback: (error, result) => void
 ) => {
-  await DbPool.query(
-    "select * from fn_get_trip($1::text);",
-    [tripId],
-    (err, res) => {
-      if (err) {
-        callback(err, null);
-      } else {
-        callback(null, res);
-      }
-    }
-  );
+  // await DbPool.query(
+  //   "select * from fn_get_trip($1::text);",
+  //   [tripId],
+  //   (err, res) => {
+  //     if (err) {
+  //       callback(err, null);
+  //     } else {
+  //       callback(null, res);
+  //     }
+  //   }
+  // );
 };
 
 export const UpdateTrip = async (
   trip: Trip,
   callback: (error, result) => void
 ) => {
-  await DbPool.query(
-    `CALL public.sp_update_trip($1::text,$2::text,$3::json,$4::text,$5::text,$6::text,$7::date,$8::text)`,
-    [
-      trip.tripId,
-      trip.registrationNumber,
-      JSON.stringify(trip.passenger),
-      trip.vehicleId,
-      trip.businesId,
-      trip.driverId,
-      trip.date,
-      trip.time,
-    ],
-    (err, res) => {
-      if (err) {
-        callback(err, null);
-      } else {
-        callback(null, res);
-      }
-    }
-  );
+  // await DbPool.query(
+  //   `CALL public.sp_update_trip($1::text,$2::text,$3::json,$4::text,$5::text,$6::text,$7::date,$8::text)`,
+  //   [
+  //     trip.tripId,
+  //     trip.registrationNumber,
+  //     JSON.stringify(trip.passenger),
+  //     trip.vehicleId,
+  //     trip.businesId,
+  //     trip.driverId,
+  //     trip.date,
+  //     trip.time,
+  //   ],
+  //   (err, res) => {
+  //     if (err) {
+  //       callback(err, null);
+  //     } else {
+  //       callback(null, res);
+  //     }
+  //   }
+  // );
 };
 
 export const UpdateTripPassengers = async (
   trip: any,
   callback: (error, result) => void
 ) => {
-  console.log(trip);
-  await DbPool.query(
-    `CALL public.sp_update_trip_passenger_status($1::text,$2::json)`,
-    [trip.tripId, JSON.stringify(trip.passenger)],
-    (err, res) => {
-      if (err) {
-        callback(err, null);
-      } else {
-        callback(null, res);
-      }
-    }
-  );
+  // console.log(trip);
+  // await DbPool.query(
+  //   `CALL public.sp_update_trip_passenger_status($1::text,$2::json)`,
+  //   [trip.tripId, JSON.stringify(trip.passenger)],
+  //   (err, res) => {
+  //     if (err) {
+  //       callback(err, null);
+  //     } else {
+  //       callback(null, res);
+  //     }
+  //   }
+  // );
 };
 
 export const GetPastTripsByBusinessIdAndParentId = async (

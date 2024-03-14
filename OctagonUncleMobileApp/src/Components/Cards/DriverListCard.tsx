5f0d59d79d9184bab7524fa813d8cc10a6f0161e
Warona Mogolwane <warona@myactive.co.za>
2024-03-15 00:27:41 +0200

    Updated driver card list

--- OctagonUncleMobileApp/src/Components/Cards/DriverListCard.tsx
+++ OctagonUncleMobileApp/src/Components/Cards/DriverListCard.tsx
@@ -22,6 +22,10 @@ import {
   Image,
   Divider,
   Pressable,
+  Avatar,
+  AvatarFallbackText,
+  AvatarImage,
+  VStack,
 } from '@gluestack-ui/themed';
 import {CustomButton1, CustomButton3} from '../Buttons';
 import {GestureResponderEvent} from 'react-native';
@@ -102,56 +106,26 @@ export const DriverListCard = (props: {
   ) => void;
 }) => {
   return (
-    <Box
-      minWidth={230}
-      minHeight={100}
-      flex={1}
-      sx={{
-        borderColor: '$trueGray300',
-        backgroundColor: '#ffffffbd',
-
-        _dark: {
-          bg: '#071117',
-          borderColor: '$borderDark800',
-        },
-      }}>
-      <Box
-        flex={4}
-        px="$3"
-        w="$full"
-        justifyContent="center"
-        sx={{
-          borderBottomColor: '$trueGray300',
-          backgroundColor: '#ffffff40',
-          _dark: {
-            borderBottomColor: '$borderDark800',
-            bg: '#0C0E12',
-          },
-        }}>
-        <Pressable onPress={props.handleDriverCardPress}>
-          <View style={{flexDirection: 'row'}}>
-            <View style={{width: '75%'}}>
-              <Heading mb="$1" size="sm">
-                {props.firstName + ' ' + props.lastName}
-              </Heading>
-              <Text size="xs">{props.email}</Text>
-              <Text size="sm">
-                {'License number: ' + props.vehicleLicenseNumber}
-              </Text>
-            </View>
-            <View>
-              <Image
-                source="https://www.english-heritage.org.uk/siteassets/home/learn/histories/black-history/cunningham-hub-item-image.jpg"
-                alt="Driver profile image"
-                height={75}
-                width={75}
-                borderRadius={50}
-              />
-            </View>
-          </View>
-          <Divider my="$2" />
-        </Pressable>
-      </Box>
-    </Box>
+    <Pressable onPress={props.handleDriverCardPress}>
+      <HStack space="md">
+        <Avatar>
+          <AvatarFallbackText>Profile Picture</AvatarFallbackText>
+          <AvatarImage
+            source={{
+              uri: 'https://media.licdn.com/dms/image/C4D03AQFotIRK58pRNA/profile-displayphoto-shrink_200_200/0/1525163555622?e=2147483647&v=beta&t=lvummEevyaevcll0SjNg8UvthCNqz05ate3HonR4zfc',
+            }}
+            alt="Driver profile picture."
+          />
+        </Avatar>
+        <VStack>
+          <Heading size="sm"> {props.firstName + ' ' + props.lastName}</Heading>
+          <Text size="sm">{props.email}</Text>
+          <Text size="sm">
+            {'License number: ' + props.vehicleLicenseNumber}
+          </Text>
+        </VStack>
+      </HStack>
+      <Divider my="$3" />
+    </Pressable>
   );
 };

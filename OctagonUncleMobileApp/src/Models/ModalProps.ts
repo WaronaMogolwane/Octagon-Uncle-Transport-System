ec93d2d5f3f7e13d59249b0db878d6c8fa8f6540
Warona Mogolwane <warona@myactive.co.za>
2024-03-14 03:02:39 +0200

    Addd driverd details props

--- OctagonUncleMobileApp/src/Models/ModalProps.ts
+++ OctagonUncleMobileApp/src/Models/ModalProps.ts
@@ -34,3 +34,27 @@ export type InvitationModalProps = {
     ) => void;
     CloseOtpModalButtonOnPress: () => void;
 };
+export type DriverDetailsModalProps = {
+    ShowModal: boolean;
+    firstNameIsInvalid: boolean;
+    firstNameOnChangeText: (e: string | ChangeEvent<any>) => void;
+    firstNameErrorText: string | undefined;
+    firstNameOnBlur: (e: any) => void;
+    firstNameValue: string;
+    lastNameIsInvalid: boolean;
+    lastNameOnChangeText: (e: string | ChangeEvent<any>) => void;
+    lastNameErrorText: string | undefined;
+    lastNameOnBlur: (e: any) => void;
+    lastNameValue: string;
+    emailIsInvalid: boolean;
+    emailOnChangeText: (e: string | ChangeEvent<any>) => void;
+    emailErrorText: string | undefined;
+    emailOnBlur: (e: any) => void;
+    emailValue: string;
+    profilePictureUrl: string;
+    vehicleLicenseNumber: string;
+    HandleRemoveDriver: (
+        values: GestureResponderEvent | React.FormEvent<HTMLFormElement> | undefined
+    ) => void;
+    CloseOtpModalButtonOnPress: () => void;
+};

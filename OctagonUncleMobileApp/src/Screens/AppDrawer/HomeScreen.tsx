421b1c0d7847625c77224b63499b51239d491eca
Iamtallers <iamtallers@gmail.com>
2024-05-01 21:08:25 +0200

    Hide Nav Drawer Elements

--- OctagonUncleMobileApp/src/Screens/AppDrawer/HomeScreen.tsx
+++ OctagonUncleMobileApp/src/Screens/AppDrawer/HomeScreen.tsx
@@ -18,8 +18,9 @@ const HomeScreen = ({navigation}: any) => {
   useEffect(() => {
     new Auth().SetAuthentication(authToken!, auth, setAuth);
     if (auth !== null) {
-      setGlobalState('userId', 'c7728615-394f-466b-833e-ea9dd60ba836');
-      setGlobalState('role', 2);
+      //setGlobalState('userId', 'c7728615-394f-466b-833e-ea9dd60ba836');
+      setGlobalState('userId', '9a7fc15e-a31a-44a5-8d1d-0a9509f7f916');
+      setGlobalState('role', 3);
       setGlobalState('businessId', 'w8728321-394f-466b-833e-ea9dd60ba000');
 
       // setGlobalState(

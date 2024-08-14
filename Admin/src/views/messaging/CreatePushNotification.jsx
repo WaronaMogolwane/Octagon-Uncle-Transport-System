58373686a6ef5ac987179e86541a309c2e5d0a9b
Warona <mogolwanew@gmail.com>
2024-08-14 15:27:49 +0200

    Push notifications updates

--- Admin/src/views/messaging/CreatePushNotification.jsx
+++ Admin/src/views/messaging/CreatePushNotification.jsx
@@ -5,6 +5,7 @@ import {
   Card,
   Form,
   Button,
+  Image,
   InputGroup,
   FormControl,
   DropdownButton,
@@ -21,8 +22,8 @@ const CreatePushNotification = () => {
               <Card.Title as="h5">Create New Push Notification</Card.Title>
             </Card.Header>
             <Card.Body>
-              <Row>
-                <Form>
+              <Form>
+                <Row>
                   <Col md={6}>
                     <Form.Group
                       className="mb-3"
@@ -35,22 +36,38 @@ const CreatePushNotification = () => {
                       className="mb-3"
                       controlId="exampleForm.ControlSelect1"
                     >
-                      <Form.Label>Select Channel</Form.Label>
+                      <Form.Label>Select channel</Form.Label>
                       <Form.Control as="select">
                         <option>General</option>
+                        <option>Clients</option>
+                        <option>Drivers</option>
+                        <option>Transporters</option>
                       </Form.Control>
                     </Form.Group>
                     <Form.Group
                       className="mb-3"
                       controlId="exampleForm.ControlTextarea1"
                     >
-                      <Form.Label>Push Notification Message</Form.Label>
+                      <Form.Label>Push notification message</Form.Label>
                       <Form.Control as="textarea" rows="3" />
+                    </Form.Group>
+                    <Form.Group controlId="formFile" className="mb-3">
+                      <Form.Label>Image</Form.Label>
+                      <Form.Control type="file" />
+                    </Form.Group>
+                  </Col>
+                  <Col md={6}>
+                    <Form.Group class="text-align-center">
+                      <Form.Label>Image preview</Form.Label>
+                      <Image
+                        src="https://f005.backblazeb2.com/file/Dev-Octagon-Uncle-Transport/Resources/Images/Octagon+Icon+Logo.png"
+                        fluid
+                      />
                     </Form.Group>
                   </Col>
                   <Button variant="primary">Submit</Button>
-                </Form>
-              </Row>
+                </Row>
+              </Form>
             </Card.Body>
           </Card>
         </Col>

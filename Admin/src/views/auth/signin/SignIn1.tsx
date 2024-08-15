212f2ded0e01664dcd6aac5e3fe84ebb98645735
Warona <mogolwanew@gmail.com>
2024-08-15 13:08:52 +0200

    Admin push notifications config

--- Admin/src/views/auth/signin/SignIn1.tsx
+++ Admin/src/views/auth/signin/SignIn1.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Card, Button, Alert } from 'react-bootstrap';
-import { NavLink, Link } from 'react-router-dom';
+import React from "react";
+import { Card, Button, Alert } from "react-bootstrap";
+import { NavLink, Link } from "react-router-dom";
 
-import Breadcrumb from '../../../layouts/AdminLayout/Breadcrumb';
+import Breadcrumb from "../../../layouts/AdminLayout/Breadcrumb";
 
-import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
-import AuthLogin from './JWTLogin';
+import AuthLogin from "./JWTLogin";
 
 const Signin1 = () => {
   return (
@@ -27,13 +27,13 @@ const Signin1 = () => {
               </div>
               <AuthLogin />
               <p className="mb-2 text-muted">
-                Forgot password?{' '}
-                <NavLink to={'#'} className="f-w-400">
+                Forgot password?{" "}
+                <NavLink to={"#"} className="f-w-400">
                   Reset
                 </NavLink>
               </p>
               <p className="mb-0 text-muted">
-                Don’t have an account?{' '}
+                Don’t have an account?{" "}
                 <NavLink to="/auth/signup-1" className="f-w-400">
                   Signup
                 </NavLink>
@@ -41,14 +41,26 @@ const Signin1 = () => {
               <Alert variant="primary" className="text-start mt-3">
                 User:
                 <CopyToClipboard text="info@codedthemes.com">
-                  <Button variant="outline-primary" as={Link} to="#" className="badge mx-2 mb-2" size="sm">
+                  <Button
+                    variant="outline-primary"
+                    as={Link}
+                    to="#"
+                    className="badge mx-2 mb-2"
+                    size="sm"
+                  >
                     <i className="fa fa-user" /> info@codedthemes.com
                   </Button>
                 </CopyToClipboard>
                 <br />
                 Password:
                 <CopyToClipboard text="123456">
-                  <Button variant="outline-primary" as={Link} to="#" className="badge mx-2" size="sm">
+                  <Button
+                    variant="outline-primary"
+                    as={Link}
+                    to="#"
+                    className="badge mx-2"
+                    size="sm"
+                  >
                     <i className="fa fa-lock" /> 123456
                   </Button>
                 </CopyToClipboard>
--- Admin/src/views/messaging/CreatePushNotification.tsx
+++ Admin/src/views/messaging/CreatePushNotification.tsx
@@ -13,7 +13,54 @@ import {
 } from "react-bootstrap";
 import { IconBase } from "react-icons";
 import { Trash2 } from "react-feather";
-const CreatePushNotification = () => {
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { SendPushNotification } from "../../models/PushNotificationModel";
+import { PushNotificationObj } from "../../classes/PushNotification";
+const CreatePushNotification = (props: any) => {
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      channel: "",
+      message: "",
+    },
+
+    // validationSchema: Yup.object({
+    //   username: Yup.string()
+    //     .min(2, "Must be at least 2 characters long")
+    //     .max(15, "Must be 15 characters or less")
+    //     .required("Required"),
+    //   email: Yup.string().email("Invalid email address").required("Required"),
+    //   password: Yup.string()
+    //     .min(6, "Must be at least 6 characters long")
+    //     .required("Required"),
+    // }),
+    onSubmit: async (values) => {
+      let pushNotification: PushNotificationObj = {
+        body: values.message,
+        title: values.title,
+        channelId: values.channel,
+      };
+      SendPushNotification(pushNotification, (error, result) => {
+        if (error) {
+          console.error(error);
+        } else {
+          formik.resetForm();
+          console.log(result);
+        }
+      });
+    },
+  });
+  const {
+    values,
+    errors,
+    dirty,
+    isSubmitting,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    handleReset,
+  } = props;
   return (
     <React.Fragment>
       <Row>
@@ -23,34 +70,47 @@ const CreatePushNotification = () => {
               <Card.Title as="h5">Create New Push Notification</Card.Title>
             </Card.Header>
             <Card.Body>
-              <Form>
+              <Form onSubmit={formik.handleSubmit}>
                 <Row>
                   <Col md={6}>
-                    <Form.Group
-                      className="mb-3"
-                      controlId="exampleForm.ControlInput1"
-                    >
+                    <Form.Group className="mb-3">
                       <Form.Label>Title</Form.Label>
-                      <Form.Control type="text" placeholder="Title" />
+                      <Form.Control
+                        type="text"
+                        placeholder="Title"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        value={formik.values.title}
+                        id="title"
+                      />
                     </Form.Group>
-                    <Form.Group
-                      className="mb-3"
-                      controlId="exampleForm.ControlSelect1"
-                    >
+                    <Form.Group className="mb-3">
                       <Form.Label>Select channel</Form.Label>
-                      <Form.Control as="select">
-                        <option>General</option>
-                        <option>Clients</option>
-                        <option>Drivers</option>
-                        <option>Transporters</option>
+                      <Form.Control
+                        as="select"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        id="channel"
+                        value={formik.values.channel}
+                      >
+                        <option value={"General-Notifications"}>General</option>
+                        <option value={"Client-Notifications"}>Clients</option>
+                        <option value={"Driver-Notifications"}>Drivers</option>
+                        <option value={"Transporter-Notifications"}>
+                          Transporters
+                        </option>
                       </Form.Control>
                     </Form.Group>
-                    <Form.Group
-                      className="mb-3"
-                      controlId="exampleForm.ControlTextarea1"
-                    >
+                    <Form.Group className="mb-3">
                       <Form.Label>Push notification message</Form.Label>
-                      <Form.Control as="textarea" rows={3} />
+                      <Form.Control
+                        as="textarea"
+                        rows={3}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        value={formik.values.message}
+                        id="message"
+                      />
                     </Form.Group>
                     <Form.Group controlId="formFile" className="mb-3 w-50">
                       <Form.Label>Image</Form.Label>
@@ -59,7 +119,13 @@ const CreatePushNotification = () => {
                         <Trash2 color="red" />
                       </div>
                     </Form.Group>
-                    <Button variant="primary">Submit</Button>
+                    <Button
+                      type="submit"
+                      disabled={isSubmitting}
+                      variant="primary"
+                    >
+                      Submit
+                    </Button>
                   </Col>
                   <Col md={6}>
                     <Form.Group className="d-inline-flex">

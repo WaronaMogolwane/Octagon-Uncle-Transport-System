a1f4a77694de1bc4da009d27c9b0a1d7206acd90
Warona <mogolwanew@gmail.com>
2024-08-20 21:45:16 +0200

    Card tokenise handling for webhooks

--- Server/src/Controllers/PaymentsController.ts
+++ Server/src/Controllers/PaymentsController.ts
@@ -1,10 +1,13 @@
+import { CardAuthorisation } from './../Classes/CardAuthorisation';
+import { Authorization, Data, WebhookEvent } from './../Classes/WebhookEvent';
 import { NextFunction, Request, Response } from "express";
 import { Customer } from "../Classes/Customer";
-import { CreateNewPaystackCustomer } from "../Models/PaymentsModel";
+import { CreateNewPaystackCustomer, CreatePaystackTransactionLink } from "../Services/PaystackService";
+import { InsertCardAuthorisation } from '../Models/PaymentsModel';
+import { ErrorResponse } from '../Classes/ErrorResponse';
+import { stringFormat } from '../Extensions/StringExtensions';
 
-export const HandleWebhookEvent = (req: Request, res: Response, next: NextFunction) => {
 
-}
 export const CreateNewCustomer = async (req: Request, res: Response, next: NextFunction) => {
     let requestBody: any = req.body;
     let newCustomer: Customer = ({
@@ -19,7 +22,7 @@ export const CreateNewCustomer = async (req: Request, res: Response, next: NextF
         userId: requestBody.userId,
         isActive: '1'
     })
-    await CreateNewPaystackCustomer(newCustomer, (error, result) => {
+    await CreateNewPaystackCustomer(newCustomer, (error: any, result: any) => {
         if (error) {
             res.status(400).json(error);
         }
@@ -30,14 +33,37 @@ export const CreateNewCustomer = async (req: Request, res: Response, next: NextF
 
 }
 export const CreateNewPlan = (req: Request, res: Response, next: NextFunction) => {
-
 }
 export const CreateNewSubscription = (req: Request, res: Response, next: NextFunction) => {
-
 }
 export const CreateTransactionLink = (req: Request, res: Response, next: NextFunction) => {
+}
+export const CreateNewCardAuthorisation = async (webhookEvent: WebhookEvent, req: Request, res: Response, next: NextFunction) => {
+    const data: Data = webhookEvent.data;
+    const authorisation: Authorization = data.authorization;
+    const cardAuthorisation: CardAuthorisation = ({
+        cardAuthorisationId: "",
+        userId: data.metadata.custom_fields[0].user_id,
+        authorizationCode: authorisation.authorization_code,
+        maskedCardNumber: stringFormat("******{0}", authorisation.last4),
+        cardExpiryMonth: authorisation.exp_month,
+        cardExpiryYear: authorisation.exp_year,
+        cardType: authorisation.card_type,
+        bankName: authorisation.bank,
+        countryCode: authorisation.country_code,
+        dateCreated: data.created_at
+    });
 
+    await InsertCardAuthorisation(cardAuthorisation, (error: any, result: any) => {
+        if (error) {
+            const err: ErrorResponse = ({ status: 400, message: error })
+            next(err);
+        }
+        else {
+            res.status(200).send({ message: "Card authorisation successfully added." })
+        }
+
+    })
 }
 export const ChargeAuthorization = (req: Request, res: Response, next: NextFunction) => {
-
 }

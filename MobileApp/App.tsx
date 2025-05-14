9e7a220dbea521dd2047553caf6e23e9cd55bf84
WaronaMogolwane <mogolwanew@gmail.com>
2025-05-15 01:49:46 +0200

    Added Sentry and PM2

--- MobileApp/App.tsx
+++ MobileApp/App.tsx
@@ -7,6 +7,26 @@ import AppNav from './src/Navigation/AppNavigation';
 import {GluestackUIProvider} from '@gluestack-ui/themed';
 import {config} from '@gluestack-ui/config';
 import {Camera} from 'react-native-vision-camera';
+import * as Sentry from '@sentry/react-native'; // Correct Sentry import for React Native
+
+Sentry.init({
+  dsn: 'https://e5b90a0e97e12c4a3ded6972b0e831bb@o4509321995616256.ingest.de.sentry.io/4509322002235472',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Configure Session Replay
+  //replaysSessionSampleRate: 0.1,
+  //replaysOnErrorSampleRate: 1,
+  integrations: [
+    Sentry.mobileReplayIntegration(),
+    Sentry.reactNativeTracingIntegration(),
+  ],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
 function App(): JSX.Element {
   //const [cameraPermission, setCameraPermission] = useState<any>();
   const [pushNotificationsPermission, setPushNotificationsPermission] =
@@ -35,4 +55,4 @@ function App(): JSX.Element {
     </SessionProvider>
   );
 }
-export default App;
+export default Sentry.wrap(App);
